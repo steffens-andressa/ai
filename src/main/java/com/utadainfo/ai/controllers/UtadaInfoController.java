3a2c4aea1f36936f336cf9785049383179a7b653
Andressa Steffens <steffensand@gmail.com>
2024-04-25 20:08:11 -0300

    Create an endpoint to get the best selling albums returning ChatResponse

--- src/main/java/com/utadainfo/ai/controllers/UtadaInfoController.java
+++ src/main/java/com/utadainfo/ai/controllers/UtadaInfoController.java
@@ -1,5 +1,7 @@
 package com.utadainfo.ai.controllers;
 
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +22,11 @@ public class UtadaInfoController {
     public String aboutChat(@RequestParam (value = "message",
             defaultValue = "Send a brief description about Utada Hikaru and the official website") String message){
         return chatClient.call(message);
+    }
+
+    @GetMapping("/top-albums")
+    public ChatResponse topAlbumsChat(@RequestParam (value = "message",
+            defaultValue = "What are the best selling albums from Utada Hikaru?") String message){
+        return chatClient.call(new Prompt(message));
     }
 }
