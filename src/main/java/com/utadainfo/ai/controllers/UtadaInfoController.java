3470870e027208ed5412f0c71764c504f60f91b7
Andressa Steffens <steffensand@gmail.com>
2024-04-25 22:01:43 -0300

    Create endpoints to translate a track and get info about a title choosing between Single or Album.

--- src/main/java/com/utadainfo/ai/controllers/UtadaInfoController.java
+++ src/main/java/com/utadainfo/ai/controllers/UtadaInfoController.java
@@ -1,9 +1,13 @@
 package com.utadainfo.ai.controllers;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Content;
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,5 +32,25 @@ public class UtadaInfoController {
     public ChatResponse topAlbumsChat(@RequestParam (value = "message",
             defaultValue = "What are the best selling albums from Utada Hikaru?") String message){
         return chatClient.call(new Prompt(message));
+    }
+
+    @GetMapping("/title-info")
+    public String titleInfoChat(@Parameter (schema = @Schema(allowableValues = {"Single", "Album"})) String type,
+                                @RequestParam (value = "title", defaultValue = "First Love") String title) {
+        PromptTemplate promptTemplate = new PromptTemplate("""
+                Send me a brief summary about the {type} {title} from Utada Hikaru and list all the tracks.
+                """);
+        promptTemplate.add("type", type);
+        promptTemplate.add("title", title);
+        return this.chatClient.call(promptTemplate.create()).getResult().getOutput().getContent();
+    }
+
+    @GetMapping("/track-translate")
+    public String translateTrackChat(@RequestParam (value = "track", defaultValue = "First Love") String track) {
+        PromptTemplate promptTemplate = new PromptTemplate("""
+                Send the lyrics in romaji and translate this track {track} from Utada Hikaru
+                """);
+        promptTemplate.add("track", track);
+        return this.chatClient.call(promptTemplate.create()).getResult().getOutput().getContent();
     }
 }
